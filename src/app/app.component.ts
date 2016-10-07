97c18f7f305fc8b951f39ca32dbe26bb5a827315
Can Kattw <kattwinkel@w11k.de>
2016-10-07 11:16:44 +0200

    Step 3: Chose a Animation and prepare it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, trigger, keyframes, animate, transition} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('wobble', [
+      transition('inactive => active', animate(1000, keyframes([
+      ]))),
+    ])
+  ]
+
 })
 export class AppComponent {
   title = 'app works!';
