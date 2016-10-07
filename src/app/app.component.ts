deb14f5afab5ff4325200d6caefaf729cf6f9ad0
Can Kattw <kattwinkel@w11k.de>
2016-10-07 11:20:34 +0200

    Step 4: Add Keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, trigger, keyframes, animate, transition} from '@angular/core';
+import {Component, trigger, keyframes, animate, transition, style} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,12 @@ import {Component, trigger, keyframes, animate, transition} from '@angular/core'
   animations: [
     trigger('wobble', [
       transition('inactive => active', animate(1000, keyframes([
+        style({transform: 'translate3d(-25%, 0, 0) rotate3d(0, 0, 1, -5deg)', offset: .15}),
+        style({transform: 'translate3d(20%, 0, 0) rotate3d(0, 0, 1, 3deg)', offset: .30}),
+        style({transform: 'translate3d(-15%, 0, 0) rotate3d(0, 0, 1, -3deg)', offset: .45}),
+        style({transform: 'translate3d(10%, 0, 0) rotate3d(0, 0, 1, 2deg)', offset: .60}),
+        style({transform: 'translate3d(-5%, 0, 0) rotate3d(0, 0, 1, -1deg)', offset: .75}),
+        style({transform: 'none', offset: 1}),
       ]))),
     ])
   ]
